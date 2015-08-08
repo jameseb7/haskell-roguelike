6340ade83a3032b30f24e93a5931c53c4ea8756e
James Baxter <jebaxter@hotmail.co.uk>
2015-08-08 19:24:40 +0100

    Add some more entity modification functions

--- HaskellRoguelike/Entity.hs
+++ HaskellRoguelike/Entity.hs
@@ -22,6 +22,7 @@ module HaskellRoguelike.Entity where
           entitySize :: EntitySize,
           entityProperties :: Set EntityProperty,
           ai :: Maybe AI
+          inventory :: Maybe [EntityID]
         }
 
     blankEntity :: (Monad m) => EntityIDGenT m Entity
@@ -35,13 +36,40 @@ module HaskellRoguelike.Entity where
                                   ai = Nothing
                                 }
 
-    hasProperty :: Entity -> EntityProperty -> Bool
-    hasProperty e p = Set.member p $ entityProperties e
+    hasProperty :: EntityProperty -> Entity -> Bool
+    hasProperty p e = Set.member p $ entityProperties e
 
     entitySymbol :: Entity -> Maybe EntitySymbol
     entitySymbol e
         | e `hasProperty` Invisible = Nothing
         | otherwise = Just $ baseEntitySymbol e
+
+    setProperty :: EntityProperty -> Entity -> Entity
+    setProperty p e = e{entityProperties = Set.insert p $ entityProperties e}
+
+    clearProperty :: EntityProperty -> Entity -> Entity
+    clearProperty p e = e{entityProperties = Set.delete p $ entityProperties e}
+
+    setXYPosition :: (Int,Int) -> Entity -> Entity
+    setXYPosition pos e = e{position = Left pos}
+
+    -- This function fails if the container identifier is the identifier of the
+    -- entity being updated
+    setContainer :: EntityID -> Entity -> Entity
+    setContainer c e
+        | c != entityID e = e{position = Right c}
+        | otherwise = error "setContainer: can't insert an entity into itself"
+
+    -- This function fails if the entity identifier being inserted is the same
+    -- as the identifier of the entity being inserted into or if the entity
+    -- being inserted into has no inventory
+    insertEntity :: EntityID -> Entity -> Entity
+    insertEntity eid e
+        | eid != entityID e = e{inventory = nub $ insert eid <$> inventory e}
+        | otherwise = error "insertEntity: can't insert an entity into itself"
+
+    removeEntity :: EntityID -> Entity -> Entity
+    removeEntity eid e = e{inventory = delete eid <$> inventory e}
 
     testEntity :: (Monad m) => EntityIDGenT m Entity
     testEntity = do e <- blankEntity
